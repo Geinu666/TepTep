aab00d8cc5fc2c3e498acd3fb855637aed1e554c
Geinu666 <51981331+Geinu666@users.noreply.github.com>
2021-07-05 23:19:29 +0800

    增加动态获取本地图片

--- SplashDemo/app/src/main/java/com/example/splashdemo/WebViewActivity.java
+++ SplashDemo/app/src/main/java/com/example/splashdemo/WebViewActivity.java
@@ -1,12 +1,15 @@
 package com.example.splashdemo;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.renderscript.Sampler;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.Gravity;
@@ -15,6 +18,8 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.PopupWindow;
@@ -32,6 +37,9 @@ public class WebViewActivity extends AppCompatActivity {
     private String func;
     private FloatingActionButton button;
     private PopupWindow popupWindow;
+
+    private ValueCallback<Uri> uploadFile;
+    private ValueCallback<Uri[]> uploadFiles;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -65,6 +73,62 @@ public class WebViewActivity extends AppCompatActivity {
         WebUtil webUtil = new WebUtil(webView, getApplicationContext());
         webUtil.webViewSetting(url, 1);
         webView.addJavascriptInterface(new JsJavaBridge(this, webView, getApplicationContext()), "$App");
+
+        webView.setWebChromeClient(new WebChromeClient() {
+            public boolean onShowFileChooser(WebView webView,
+                                             ValueCallback<Uri[]> filePathCallback,
+                                             WebChromeClient.FileChooserParams fileChooserParams) {
+                uploadFiles = filePathCallback;
+                openFileChooseProcess();
+                return true;
+            }
+
+            public void openFileChooser(ValueCallback<Uri> uploadMsg, String acceptType, String capture) {
+                uploadFile = uploadFile;
+                openFileChooseProcess();
+            }
+        });
+    }
+
+    private void openFileChooseProcess() {
+        Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+        i.addCategory(Intent.CATEGORY_OPENABLE);
+        i.setType("*/*");
+        startActivityForResult(Intent.createChooser(i, "选择文件"), 0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            switch (requestCode) {
+                case 0:
+                    if (uploadFile != null) {
+                        Uri result = data == null || resultCode != RESULT_OK ? null
+                                : data.getData();
+                        uploadFile.onReceiveValue(result);
+                        uploadFile = null;
+                    }
+                    if (uploadFiles != null) {
+                        Uri result = data == null || resultCode != RESULT_OK ? null
+                                : data.getData();
+                        uploadFiles.onReceiveValue(new Uri[]{result});
+                        uploadFiles = null;
+                    }
+                    break;
+                default:
+                    break;
+            }
+        } else if (resultCode == RESULT_CANCELED) {
+            if (uploadFile != null) {
+                uploadFile.onReceiveValue(null);
+                uploadFile = null;
+            }
+            if (uploadFiles != null) {
+                uploadFiles.onReceiveValue(null);
+                uploadFiles = null;
+            }
+        }
     }
 
     @Override
